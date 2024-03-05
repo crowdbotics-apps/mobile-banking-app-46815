3029c9836f4a2448ba01fae67ccc951ff94619d2
Crowdbotics <team@crowdbotics.com>
2024-03-05 13:02:52 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mobile-banking-app-46815",
+  url: "https://mobile-banking-app-46815.botics.co",
+  api: "https://mobile-banking-app-46815.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
